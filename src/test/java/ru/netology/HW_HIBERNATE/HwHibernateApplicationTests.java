5398dd16e5f38b8db301797cf6007a4ac2005738
Левыкин Алексей Александрович <alevykin@alfabank.ru>
2025-08-01 22:00:25 +0500

    init

---
+++ src/test/java/ru/netology/HW_HIBERNATE/HwHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.HW_HIBERNATE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
