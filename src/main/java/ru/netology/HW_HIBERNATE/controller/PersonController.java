a202f2a8d6d7d89c501de51038ec07c4faa084bf
Levykin <a.a.levykin@gmail.com>
2025-08-24 01:17:02 +0500

    Задание выполнено

--- src/main/java/ru/netology/HW_HIBERNATE/controller/PersonController.java
+++ src/main/java/ru/netology/HW_HIBERNATE/controller/PersonController.java
@@ -1,6 +1,5 @@
 package ru.netology.HW_HIBERNATE.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
